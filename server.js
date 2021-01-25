3845188681756db353ac748921c7b6dfc7628a2e
Efrain Vazquez <efras1202@hotmail.com>
2021-01-25 04:55:31 -0600

    Heroku app

--- server.js
+++ server.js
@@ -3,9 +3,9 @@ const path = require('path');
 
 const app = express();
 
-app.use(express.static(__dirname + '/dist/EXAMENFRONT'));
+app.use(express.static(__dirname + '/dist/ExamenFront'));
 app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname + '/dist/EXAMENFRONT/index.html'));
+    res.sendFile(path.join(__dirname + '/dist/ExamenFront/index.html'));
 });
 
 app.listen(process.env.PORT || 8080);
